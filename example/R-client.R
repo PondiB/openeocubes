e283d83f5641079dd554d89821eabb6502d9c903
Brian Pondi <brianpondi.geogeek@gmail.com>
2022-07-19 12:12:48 +0200

    Update example file

--- example/R-client.R
+++ example/R-client.R
@@ -12,9 +12,16 @@ login(user = "user",
 p = processes()
 
 # load the initial data collection and limit the amount of data loaded
-data = p$load_collection(id = "sentinel-s2-l2a-cogs",
+data.cube = p$load_collection(id = "sentinel-s2-l2a-cogs",
                          spatial_extent = list(west=16.06,
                                                south=48.06,
                                                east=16.65,
                                                north=48.35),
                          temporal_extent = c("2021-01-01", "2021-06-30"))
+
+# filter the data cube for the desired bands
+data.cube= p$filter_bands(data = data.cube, bands = c("B04", "B08"))
+
+
+
+## TO Do  -> reducer UDF vs apply per pixel UDF
