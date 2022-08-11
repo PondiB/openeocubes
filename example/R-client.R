fe42c9e77d8d3c5a38f45ad3dfb0c31371bf1a9f
Brian Pondi <brianpondi.geogeek@gmail.com>
2022-08-11 20:58:56 +0200

    Redine example

--- example/R-client.R
+++ example/R-client.R
@@ -12,13 +12,13 @@ login(user = "user",
 collections = list_collections()
 
 # print an overview of the available collections (printed as data.frame or tibble)
-print(collections)
+collections
 
 # to check description of a collection
 collections$`sentinel-s2-l2a-cogs`$description
 
-# to check available processes and their descriptions
-processes = list_processes()
+# Check that required processes are available ,if you remove the indexing you will get all processes visualized.
+list_processes()[1:5]
 
 # to check specific process e.g. ndvi
 describe_process(processes$ndvi)
@@ -27,7 +27,7 @@ describe_process(processes$ndvi)
 p = processes()
 
 # load the initial data collection and limit the amount of data loaded
-data.cube = p$load_collection(id = "sentinel-s2-l2a-cogs",
+datacube_init = p$load_collection(id = "sentinel-s2-l2a-cogs",
                           spatial_extent = list(west=7.1,
                                                 south=51.8,
                                                 east=7.2,
@@ -39,16 +39,16 @@ data.cube = p$load_collection(id = "sentinel-s2-l2a-cogs",
                          )
 
 # filter the data cube for the desired bands
-data.cube = p$filter_bands(data = data.cube, bands = c("B04", "B08"))
+datacube_filtered = p$filter_bands(data = datacube_init, bands = c("B04", "B08"))
 
 # rename bands
-#data.cube = rename_dimension(data = data.cube, "B04" = "red", "B08" = "nir")
+#datacube_renamed = rename_dimension(data = datacube_filtered, "B04" = "red", "B08" = "nir")
 
 # ndvi calculation
-data.cube = p$ndvi(data = data.cube, red = "B04", nir = "B08" )
+datacube_ndvi = p$ndvi(data = datacube_filtered , red = "B04", nir = "B08" )
 
 # reducer User-Defined Function -> NDVI Trend
-ndvi.trend = "function(x) {
+ndvi_trend = "function(x) {
   z = data.frame(t=1:ncol(x), ndvi=x[\"NDVI\",])
   result = NA
   if (sum(!is.na(z$ndvi)) > 3) {
@@ -57,19 +57,19 @@ ndvi.trend = "function(x) {
   return(result)}"
 
 # run User-Defined Function
-# data.cube = p$run_udf(data = data.cube, udf = ndvi.trend)
+# datacube_ndvitrend = p$run_udf(data = datacube_ndvi, udf = ndvi_trend)
 
 # supported formats
 formats = list_file_formats()
 
 # save as GeoTiff or NetCDF
-data.cube = p$save_result(data = data.cube, format = formats$output$GTiff )
+result = p$save_result(data = data.cube, format = formats$output$GTiff )
 
 # Process and download data synchronously
-compute_result(graph = data.cube, output_file = "ndvi.tif")
+compute_result(graph = result, output_file = "ndvi.tif")
 
 # create a job
-#job = create_job(graph = data.cube, title = "ndviTrend", description = "NDVI Trend")
+#job = create_job(graph = result, title = "ndviTrend", description = "NDVI Trend")
 
 # then start the processing of the job
 #start_job(job = job)
